bcc8bf3820b076cf7b43707624db0338b1c21b16
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-07-12 02:46:39 +0400

    feat: add game constants

---
+++ src/constants.zig
@@ -0,0 +1,6 @@
+pub const SCREEN_WIDTH = 800;
+pub const SCREEN_HEIGHT = 450;
+pub const GRID_SIZE = 300;
+pub const CELL_SIZE: i32 = GRID_SIZE / 3;
+pub const GRID_X: i32 = (SCREEN_WIDTH - GRID_SIZE) / 2;
+pub const GRID_Y: i32 = (SCREEN_HEIGHT - GRID_SIZE) / 2;
